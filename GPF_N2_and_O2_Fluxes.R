f6b60361c6739701ad7c716ace587a353594a778
HollieEmery <hemery@bu.edu>
2015-12-31 14:02:54 -0500

    Plotting refinements

--- GPF_N2_and_O2_Fluxes.R
+++ GPF_N2_and_O2_Fluxes.R
@@ -47,11 +47,11 @@ colnames(O2) <- c("date","BayO2_mean","ProvO2_mean","BayO2_sderr","ProvO2_sderr"
 O2$datelabel <- format(as.Date(O2$date),format="%b-%y")
 
 #### Figure 2 ####
-
-par(mfrow = c(1,2))
+setEPS(width=190/25.4,height=115/25.4)
+postscript("Figure2.eps")
 
 #plot A - Providence River Estuary
-par(mai = c(1.4,1.4,1,0.1))
+par(fig=c(.15,.55,.2,.9),mai = c(0,0,0,0))
 mean <- N2$ProvN2_mean
 sderr <- N2$ProvN2_sderr
 names <- N2$datelabel
@@ -59,45 +59,46 @@ plotter(mean,sderr,names,"green4",c(-300,300))
 axis(2,cex.axis=.7,tcl=-.2,las=2) #adds an axis with labels
 mtext(side = 2, text = expression(Net ~ Sediment ~ N[2]-N ~ Flux ~ (mu ~ mol ~ m^{-2} ~ h^{-1})), line = 2.8, cex=.7)
 text(1,250,"a")
+text(12,-200, bquote(atop(Mean  ~ Sediment ~ N[2]-N ~ Flux , .(round(mean(mean,na.rm=T))) %+-% .(round(se(mean))) ~ (mu ~ mol ~ m^{-2} ~ h^{-1}))), cex=.7, col = "green4", adj=c(0,0))
 
 #plot B - Narragansett Bay
-par(mai=c(1.4,0.1,1,1.4))
+par(fig=c(0.55,.95,.2,.9),mai=c(0,0,0,0),new=TRUE)
 mean <- N2$BayN2_mean
 sderr <- N2$BayN2_sderr
 names <- N2$datelabel
 plotter(mean,sderr,names,"red3",c(-300,300))
 axis(2,labels=FALSE,tcl=-.2) #adds an axis without labels
 text(1,250,"b")
+text(12,-200, bquote(atop(Mean  ~ Sediment ~ N[2]-N ~ Flux , .(round(mean(mean,na.rm=T))) %+-% .(round(se(mean))) ~ (mu ~ mol ~ m^{-2} ~ h^{-1}))), cex=.7, col = "red3", adj =c(0,0))
 
-par(mfrow = c(1,1))
-
+dev.off()
 
 #### Figure 3 ####
-
-#This is to make a similar figure to the above but with O2 
-par(mfrow = c(1,2))
+setEPS(width=190/25.4,height=115/25.4)
+postscript("Figure3.eps")
 
 #plot A - Providence River Estuary
-par(mai=c(1.4,1.4,1,0.1))
+par(fig=c(.15,.55,.2,.9),mai = c(0,0,0,0))
 mean <- O2$ProvO2_mean
 sderr <- O2$ProvO2_sderr
 names <- O2$datelabel
 plotter(mean,sderr,names,"green4",c(0,8000))
 axis(2,cex.axis=.7,tcl=-.2,las=2) #adds an axis with labels
 mtext(side = 2, text = expression(Sediment ~ O[2] ~ Demand ~ (mu ~ mol ~ m^{-2} ~ h^{-1})), line = 2.8, cex=.7)
-text(1,7000,"a")
+text(1,7000,"a",adj=c(0,0))
+text(12,7000,bquote(atop(Mean  ~ Sediment ~ O[2] ~ Demand , .(round(mean(mean,na.rm=T))) %+-% .(round(se(mean))) ~ (mu ~ mol ~ m^{-2} ~ h^{-1}))), cex=.7, col = "green4",adj = c(0,0))
 
 #plot B - Narragansett Bay
-par(mai=c(1.4,0.1,1,1.4))
+par(fig=c(0.55,.95,.2,.9),mai=c(0,0,0,0),new=TRUE)
 mean <- O2$BayO2_mean
 sderr <- O2$BayO2_sderr
 names <- O2$datelabel
 plotter(mean,sderr,names,"red3",c(0,8000))
 axis(2,labels=FALSE,tcl=-.2) #adds an axis without labels
-text(1,7000,"b")
+text(1,7000,"b", adj=c(0,0))
+text(12,7000,bquote(atop(Mean  ~ Sediment ~ O[2] ~ Demand , .(round(mean(mean,na.rm=T))) %+-% .(round(se(mean))) ~ (mu ~ mol ~ m^{-2} ~ h^{-1}))), cex=.7, col = "red3",adj = c(0,0))
 
-par(mfrow = c(1,1))
-
+#dev.off()
 
 #### Figure 4 ####
 
@@ -108,8 +109,13 @@ N2_fit = lm(mimsdata$N2Flux~lmo2)
 #make a vector of plotting symbols
 symbol=ifelse(mimsdata[,1]=="Prov",19,ifelse(mimsdata[,1]=="Mid Bay",1,NA))
 
+setEPS(width=190/25.4,height=115/25.4)
+postscript("Figure4.eps")
 par(mai=c(1.02,0.82,0.82,0.42))
 plot(lmo2, mimsdata$N2Flux, pch=symbol,  ylim = c(-300,350),xlab="", ylab="")
 abline(N2_fit)
 mtext(side = 2, text = expression(Net ~ Sediment ~ N[2]-N ~ Flux ~ (mu ~ mol ~ m^{-2} ~ h^{-1})), line = 1.8)
 mtext(side = 1, text = expression(O[2] ~ Flux ~ (mu ~ mol ~ m^{-2} ~ h^{-1})), line = 2.5)
+options(scipen=999)
+text(4100,300,bquote(atop(y == .(round(N2_fit$coeff[2],2))*x + .(round(N2_fit$coeff[1],2)), R^{2} ==.(round(summary(N2_fit)$r.squared,2))~~~~ p ==.(round(anova(N2_fit)$'Pr(>F)'[1],4)))), adj=1) 
+dev.off()
